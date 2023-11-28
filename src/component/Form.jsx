0ab18ebd3070304f55e66ee0bcb0729759ec88db
Thuyatun <thuratundev@gmail.com>
2023-11-28 19:55:45 +0630

    todolist

---
+++ src/component/Form.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Form = () => {
+    return (
+        <div>
+            <i className='fa fa-user'></i>
+            <div className='row m-3'>
+                <div className='col-2'>
+                    <label htmlFor="">Name</label>
+                </div>
+
+                <div className='col-10'>
+                    <input type="text" className='form-control w-25' name="" id="" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Form
